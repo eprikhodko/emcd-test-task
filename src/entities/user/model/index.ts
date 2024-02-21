a065f8d42931dbc3e8e2a91be103e8dfd3f5a8e8
eprikhodko <eprikhodkodev@gmail.com>
2024-02-21 12:16:49 +0100

    add isPending state to the user model

--- src/entities/user/model/index.ts
+++ src/entities/user/model/index.ts
@@ -1,13 +1,15 @@
-import { createStore, ActionContext } from "vuex";
+import { createStore } from "vuex";
 import { fetchUsers } from "../api";
 import { User } from "./types";
 
 export interface UserState {
   users: User[];
+  isPending: boolean;
 }
 
 const state: UserState = {
   users: [],
+  isPending: false,
 };
 
 export const store = createStore<UserState>({
@@ -17,18 +19,22 @@ export const store = createStore<UserState>({
     setUsers(state: UserState, users: User[]) {
       users.forEach((user) => state.users.push(user));
     },
+    setPending(state: UserState, status: boolean) {
+      state.isPending = status;
+    },
   },
 
   actions: {
-    async loadUsers(
-      { commit }: ActionContext<UserState, UserState>,
-      query: string
-    ) {
+    async loadUsers({ commit, state }, query: string) {
+      if (state.isPending) return;
+      commit("setPending", true);
       try {
         const users = await fetchUsers(query);
         commit("setUsers", users.results);
       } catch (error) {
         console.error("Failed to load users:", error);
+      } finally {
+        commit("setPending", false);
       }
     },
   },
