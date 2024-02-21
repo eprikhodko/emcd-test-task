87eccfc9d526a8d8e59f66f796031eb84652babd
eprikhodko <eprikhodkodev@gmail.com>
2024-02-21 13:19:02 +0100

    add JSDOC comments

--- src/entities/user/model/index.ts
+++ src/entities/user/model/index.ts
@@ -2,6 +2,10 @@ import { createStore } from "vuex";
 import { fetchUsers } from "../api";
 import { User } from "./types";
 
+/**
+ * State structure for the user module.
+ */
+
 export interface UserState {
   users: User[];
   isPending: boolean;
@@ -16,15 +20,29 @@ export const store = createStore<UserState>({
   state,
 
   mutations: {
+     /**
+     * Mutates state to add new users.
+     * @param state - The current state of the user module.
+     * @param users - Array of users to add to the state.
+     */
     setUsers(state: UserState, users: User[]) {
       users.forEach((user) => state.users.push(user));
     },
+     /**
+     * Sets the loading state for users data.
+     * @param state - Current state of the user module.
+     * @param status - The new loading status.
+     */
     setPending(state: UserState, status: boolean) {
       state.isPending = status;
     },
   },
 
   actions: {
+     /**
+     * Asynchronously loads users based on a given query, then updates the state.
+     * Prevents loading if a request is already pending, to minimize unnecessary requests.
+     */
     async loadUsers({ commit, state }, query: string) {
       if (state.isPending) return;
       commit("setPending", true);
